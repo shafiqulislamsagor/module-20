67b8683fd9e4c1ce792fc6e04c9ff43abb56454a
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-03 19:11:59 +0600

    task 1

---
+++ task.js
@@ -0,0 +1,10 @@
+// Task 1
+
+function myMultiply(num1 , num2 , num3 , num4){
+    let multiply = num1*num2*num3*num4;
+    return multiply ;
+}
+
+let gunfol = myMultiply(7,8,9,5);
+
+console.log(gunfol)
